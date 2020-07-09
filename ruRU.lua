d6fbc2d6e0d3ce292e6cbd1ba1fca53bfc005f20
Hubbotu <34053496+Hubbotu@users.noreply.github.com>
2020-07-09 09:08:56 +0300

    Add files via upload

---
+++ ruRU.lua
@@ -0,0 +1,21 @@
+local ADDON, Package = ...
+
+Package.ruRU = {
+  money = "Деньги",
+  durability = DURABILITY,
+  durability_inventory = "Инвентарь",
+  durability_container = "Сумка",
+  raid_information = RAID_INFO,
+  move_speed = "Скорость Перемещения",
+  move_speed_run = "Бежать",
+  move_speed_swim = "Плавать",
+  location = "Камень возвращения и местоположение",
+  location_unavailable = "Недоступный",
+  profession_cooldown = "Кулдаун Профессии",
+  usable = "Доступный",
+  general = "Общее",
+  position = "Позиция",
+  record = "Запись",
+  ask_to_delete_record = "Вы действительно хотите удалить этот предмет и перезагрузить?",
+  reset = "Сброс"
+}
